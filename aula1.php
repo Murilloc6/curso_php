970eec09c31b15809112af5a19e47b503b125e4f
Murilloc6 <murilloscarraro@gmail.com>
2024-06-18 21:58:23 -0300

    Primeiro commit no github

---
+++ aula1.php
@@ -0,0 +1,35 @@
+<?php
+/* Aula de número 1 - Dia: 18/06/24
+Turma BG05000 
+Conteudos: Echos, coentários, variáveis
+Por: Murillo S. Carraro 
+*/
+
+// Este é um comentário do código, não será exibido em tela.
+echo "Bem vindo!<br><br>";
+
+# Outro tipo de comentário.
+
+/* Mais um tipo de comentário. */
+
+$texto = "Discurso de ódio, de quanto odeio a Iara<br><br>";
+echo $texto; 
+
+echo "Quem Odeia Ela Também: <br> - Murillo <br> - Pfvs <br> - Carol <br>";
+
+$outravariavel = "<br>Outro grande texto!"; // Este texto serve para indentificar pessoas quais não gostam da Iara também. (Comentário, não aparece no site);
+echo $outravariavel;
+
+echo "Calculos matemáticos<br>";
+echo 1+1;
+echo "<br>";
+echo 10-7;
+echo "<br>";
+echo 66*77;
+echo 0/0; // Numero zero, não está aparecendo ( Erro );
+echo '<br>';
+echo 4%2;
+echo '<br>';
+echo 5%2;
+
+?>
