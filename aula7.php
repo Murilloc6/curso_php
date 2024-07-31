5b3a9c86279d199115c20a4cf2b17038846e4001
Murilloc6 <murilloscarraro@gmail.com>
2024-07-30 22:00:45 -0300

    aula7.php

---
+++ aula7.php
@@ -0,0 +1,45 @@
+<?php
+
+$vetor = array();
+ $vetorPar = [];
+ $vetorImpar = [];
+// $i = 0, 1, 2, 3, 4
+// $tamanho = count($vetor);
+
+for ($i =1; $i <= 100; $i++)
+
+// Valida se é par - Pois Divide Por 2 
+    if ( $i % 2 == 0){
+        $somaDosPares += $i;
+        //$vetorPar = array_push($vetorPar, $i);
+        $vetorPar [] = $i;
+    } else {
+        $somaDosImpares += $i;
+       // $vetorImpar = array_push ($vetorImpar, $i++);
+        $vetorImpar [] = $i; 
+    }
+
+    echo "<strong> Impares: </strong> <br>";
+for ($i1 =0; $i1 <= count($vetorImpar); $i1++){
+    echo $vetorImpar [$i1] . ",  "; // 1, 3, 5, 7 ...
+    }
+echo "<br> <br>";
+
+echo "<strong> Pares: </strong> <br>";
+    for ($i2 =0; $i2 <= count ($vetorPar); $i2++){
+        echo $vetorPar [$i2] . ", "; // 2, 4, 6, 8 ...
+    }
+
+    echo "<strong> <br><br>Pares: </strong> <br><br><br>";
+    for ($i2 = count ($vetorPar); $i2 <= 0; $i2--){
+        echo $vetorPar [$i2] . ", "; // 2, 4, 6, 8 ...
+    }
+
+    echo "<br> <br>";
+
+    echo "<strong> Impares: </strong> <br>";
+    for ($i1 = count($vetorImpar); $i1 <= 0; $i1--){
+        echo $vetorImpar [$i1] . ", "; // 1, 3, 5, 7 ...
+        }
+ 
+
