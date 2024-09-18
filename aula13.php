01a99974d840cdd9106a2113ce4a0e18069c4acb
Murilloc6 <murilloscarraro@gmail.com>
2024-09-17 22:02:30 -0300

    aula13.php
    Calculadora

---
+++ aula13.php
@@ -0,0 +1,45 @@
+<?php
+
+
+/**
+ * criar por meio de objeto as operacoes matematicas + - * /
+ */
+
+class Calculadora {
+
+    // Propriedade || atributo
+    public $resultado = 0;
+
+    public function somar($valor, $valor2) {
+        $this->resultado = $valor + $valor2;
+    }
+
+    public function diminuir($valor, $valor2) {
+        $this->resultado = $valor - $valor2;
+    }
+
+    public function multiplicar($valor, $valor2) {
+        $this->resultado = $valor * $valor2;
+    }
+
+    public function dividir($valor, $valor2) {
+        $this->resultado = $valor / $valor2;
+    }
+
+    public function __toString()
+    {
+        return  $this->resultado;
+    }
+}
+
+$calculadora = new Calculadora();
+
+$calculadora->somar(10, 10);
+echo $calculadora->resultado;
+
+echo "<br/>";
+
+// $calculadora->dividir(10, 10);
+// echo $calculadora->resultado;
+echo $calculadora->resultado;
+echo $calculadora;
