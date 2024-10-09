0c7b68e2be20351ae4085968ba9930606f262700
Murilloc6 <murilloscarraro@gmail.com>
2024-10-08 21:36:03 -0300

    Models

---
+++ src/models/PessoaModels.php
@@ -0,0 +1,49 @@
+<?php
+
+class PessoaModels {
+
+    private $fields = [
+        'id',
+        'excluido',
+        'usuario_alteracao',
+        'data_criacao',
+        'data_atualizacao',
+        'nome',
+        'sobrenome',
+        'nome_completo',
+        'sexo',
+        'email',
+        'cpf',
+        'cnpj',
+        'tipo_pessoa',
+        'estado_civil',
+        'data_nascimento',
+        'id_usuario',
+    ];
+    
+    public function create() {
+
+    }
+
+    public function read($id) {
+
+    }
+
+    public function ReadAll() {
+
+    }
+
+    public function update($id) {
+
+    }
+    
+    public function delete($id) {
+
+    }
+
+}
+
+
+
+
+
---
+++ src/models/UsuarioModels.php
@@ -0,0 +1,26 @@
+<?php
+
+// use = require_once '';
+// npm || yarn = nodeJS(back) e React(front)
+// composer = PHP
+
+
+// Nome da classe tem q ser igual a extensão 
+// Ex: Usuario1234.php
+// class Usuario1234 {}
+
+echo "oi";
+exit;
+
+require_once './src/Conexao.php';
+
+class UsuarioModel {
+    
+
+}
+
+
+
+
+
+
